a802743a3854a64b70b1fb30be68d6416fa8ada4
khalid fifel <fifle.khalid99@gmail.com>
2023-09-24 20:19:55 +0100

    update print card logics

--- src/main/java/com/youcode/service/PrintingService.java
+++ src/main/java/com/youcode/service/PrintingService.java
@@ -3,47 +3,55 @@ package com.youcode.service;
 import com.youcode.consts.Colors;
 
 import java.io.Console;
+import java.util.List;
 
 public class PrintingService {
 
-    /**
-     * this method print the card given
-     * @param value
-     * @param symbole
-     * @return String
-     */
-    public static String printCard(int value, int symbole) {
-        String suitSymbol;
-        String cardValue;
-        String cardColor = Colors.RESET;
+    public static void printCard(List<List<Integer>> cards) {
+        String line1 = "", line2 = "", line3 = "", line4 = "", line5 = "";
+        for (List<Integer> card : cards) {
+            int value = card.get(0);
+            int symbole = card.get(1);
 
-        switch (value) {
-            case 1:  cardValue = "A";  break;
-            case 11: cardValue = "J"; break;
-            case 12: cardValue = "Q"; break;
-            case 13: cardValue = "K"; break;
-            default: cardValue = String.valueOf(value);
+            StringBuilder suitSymbol = new StringBuilder();
+            String cardValue;
+            String cardColor = Colors.RESET;
+
+            switch (value) {
+                case 1:  cardValue = "A";  break;
+                case 11: cardValue = "J"; break;
+                case 12: cardValue = "Q"; break;
+                case 13: cardValue = "K"; break;
+                default: cardValue = String.valueOf(value);
+            }
+            switch (symbole){
+                case 1: cardColor = Colors.RED;
+                        suitSymbol.append(" ♥ "); break;
+
+                case 2: suitSymbol.append(" ♣ "); break;
+
+                case 3: cardColor = Colors.RED;
+                        suitSymbol.append(" ♦ "); break;
+
+                case 4: suitSymbol.append(" ♠ "); break;
+
+                default: throw new IllegalStateException("the symbole should be 1, 2, 3 or 4");
+            }
+            if (cardValue.equals("10"))
+                suitSymbol.deleteCharAt(2);
+            line1 +=  cardColor + "+-------+   " + Colors.RESET;
+            line2 +=  cardColor + "| " + cardValue + suitSymbol + "  |   " + Colors.RESET;
+            line3 +=  cardColor + "|       |   " + Colors.RESET;
+            line4 +=  cardColor + "|       |   " + Colors.RESET;
+            line5 +=  cardColor + "+-------+   " + Colors.RESET;
+
         }
-        switch (symbole){
-            case 1: cardColor = Colors.RED;
-                    suitSymbol = " ♥"; break;
-
-            case 2: suitSymbol = " ♣"; break;
-
-            case 3: cardColor = Colors.RED;
-                    suitSymbol = " ♦"; break;
-
-            case 4: suitSymbol = " ♠"; break;
-
-            default: throw new IllegalStateException("the symbole should be 1, 2, 3 or 4");
-        }
-
-        return  cardColor +
-                "+-------+\n" +
-                "| " + cardValue + suitSymbol + "   |\n" +
-                "|       |\n" +
-                "|       |\n" +
-                "+-------+\n" + Colors.RESET;
+        System.out.println(  line1 + "\n"+
+                line2 + "\n"+
+                line3 + "\n"+
+                line4 + "\n"+
+                line5 + "\n"
+        );
     }
 
 }
