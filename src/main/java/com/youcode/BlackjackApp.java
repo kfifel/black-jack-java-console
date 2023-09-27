d83b2fd070f2fb00c82ca2eea0fed12e5bf7782c
khalid fifel <fifle.khalid99@gmail.com>
2023-09-27 09:42:50 +0100

    add primitive methods

--- src/main/java/com/youcode/BlackjackApp.java
+++ src/main/java/com/youcode/BlackjackApp.java
@@ -2,20 +2,22 @@ package com.youcode;
 
 import com.youcode.controller.BlackjackController;
 import com.youcode.service.primitive.ArraysManipulation;
+import com.youcode.service.primitive.BlackjackService;
 
 public class BlackjackApp {
     public static void main(String[] args) {
         BlackjackController app = new BlackjackController();
 
         //app.startGame();
-        int [][] table = new int[4][2];
-        for (int i = 0; i <4; i++) {
-            for (int j = 0; j < 2; j++) {
-                table[i][j] = i*j;
-            }
+        int [][] table = new int[10][2];
+        for (int i = 0; i <10; i++) {
+                table[i][0] = i*i+i+1;
+                table[i][1] = i*i+i+2;
         }
+        System.out.println(table.length);
+        ArraysManipulation.shuffle2D(table);
+        ArraysManipulation.sort2D(table);
 
-        int[][][] res = ArraysManipulation.deleteElement(table, 1);
-        System.out.println("dd");
+
     }
 }
--- src/main/java/com/youcode/service/primitive/ArraysManipulation.java
+++ src/main/java/com/youcode/service/primitive/ArraysManipulation.java
@@ -1,6 +1,23 @@
 package com.youcode.service.primitive;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class ArraysManipulation {
+    public static void sort2D(int[][] arr) {
+        int size = arr.length;
+
+        for (int i = 0; i <size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j][0] < arr[j + 1][0]) {
+                    int[] temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+    }
     public static int [][][] deleteElement(int[][] array, int index) {
         int size = array.length;
         int [][][] result = new int[2][][];
@@ -24,4 +41,30 @@ public class ArraysManipulation {
 
         return result;
     }
+
+    public static int[][] deleteByIndex2D(int[][] array, int index) {
+        int size = array.length;
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index :" + index);
+        }
+
+        for (int i = index; i < size -1; i++) {
+            array[i] = array[i+1];
+        }
+
+        return Arrays.copyOf(array, size - 1);
+    }
+
+    public static void shuffle2D (int [][] arr) {
+        Random random = new Random();
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            int index = random.nextInt(size);
+            int [] temp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
 }
